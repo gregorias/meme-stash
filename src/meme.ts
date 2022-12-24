e0262f05bf2b9c9e7199ed682b231d443d137926
Grzegorz Milka <grzegorzmilka@gmail.com>
2022-12-24 14:34:08 +0100

    feat: add more memes

--- src/meme.ts
+++ src/meme.ts
@@ -1,7 +1,9 @@
 import { StaticImageData } from "next/image";
 import angryThumbsUp from "../public/angry thumbs up.gif";
 import bigThonk from "../public/big thonk.gif";
+import coralineDad from "../public/coraline dad.gif";
 import creepyButILikeIt from "../public/creepy but I like it.gif";
+import disgustedClintEastwood from "../public/disgusted clint eastwood.gif";
 import elsaFuck from "../public/elsa fuck.gif";
 import judgeJudyWatch from "../public/judge judy watch.gif";
 import koalaGasolineExplosion from "../public/koala gasoline explosion.gif";
@@ -45,7 +47,23 @@ export class MemeDatabase {
   memes: Meme[] = [
     { gif: angryThumbsUp, tags: ["angry", "juho", "thumbs-up"] },
     { gif: bigThonk, tags: ["think", "emoji", "thonk"] },
+    {
+      gif: coralineDad,
+      tags: [
+        "coraline",
+        "father",
+        "dad",
+        "type",
+        "computer",
+        "tired",
+        "boredom",
+      ],
+    },
     { gif: creepyButILikeIt, tags: ["creepy", "like"] },
+    {
+      gif: disgustedClintEastwood,
+      tags: ["coffee", "disgust", "clint-eastwood"],
+    },
     { gif: elsaFuck, tags: ["elsa", "frozen", "fuck"] },
     { gif: judgeJudyWatch, tags: ["judge", "judy", "watch"] },
     {
